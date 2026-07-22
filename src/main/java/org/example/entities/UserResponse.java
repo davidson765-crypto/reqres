428d2aaf730bff6e393285a30cdc99e7ec7eeb3f
davidson765-crypto <dmiraev@gmail.com>
2026-07-22 14:05:05 +0300

    add lombok data deserializer

--- src/main/java/org/example/entities/UserResponse.java
+++ src/main/java/org/example/entities/UserResponse.java
@@ -1,21 +1,13 @@
 package org.example.entities;
 
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
-//@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class UserResponse {
 
     private UserData data;
 
-    public UserData getData() {
-        return data;
-    }
-
-    public void setData(UserData data) {
-        this.data = data;
-    }
-
-    //@JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
     public static class UserData {
         private String id;
         private String email;
@@ -23,16 +15,25 @@ public class UserResponse {
         private String last_name;
         private String avatar;
 
-        public void setId(String id) { this.id = id; }
-        public void setEmail(String email) { this.email = email; }
-        public void setFirst_name(String first_name) { this.first_name = first_name; }
-        public void setLast_name(String last_name) { this.last_name = last_name; }
-        public void setAvatar(String avatar) { this.avatar = avatar; }
-
-        public String getId() { return id; }
-        public String getEmail() { return email; }
-        public String getFirst_name() { return first_name; }
-        public String getLast_name() { return last_name; }
-        public String getAvatar() { return avatar; }
+        public UserData setId(String id) {
+            this.id = id;
+            return this;
+        }
+        public UserData setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+        public UserData setFirst_name(String first_name) {
+            this.first_name = first_name;
+            return this;
+        }
+        public UserData setLast_name(String last_name) {
+            this.last_name = last_name;
+            return this;
+        }
+        public UserData setAvatar(String avatar) {
+            this.avatar = avatar;
+            return this;
+        }
     }
 }
