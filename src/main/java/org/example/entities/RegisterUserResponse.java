22105345f3f09e0712f18e8629e0481730b88e30
davidson765-crypto <dmiraev@gmail.com>
2026-07-22 17:06:54 +0300

    add lombok data

--- src/main/java/org/example/entities/RegisterUserResponse.java
+++ src/main/java/org/example/entities/RegisterUserResponse.java
@@ -1,24 +1,10 @@
 package org.example.entities;
 
+import lombok.Data;
+
+@Data
 public class RegisterUserResponse {
 
     private String id;
-
     private String token;
-
-    public String getId() {
-        return id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
--- src/main/java/org/example/entities/UsersResponse.java
+++ src/main/java/org/example/entities/UsersResponse.java
@@ -1,10 +1,11 @@
 package org.example.entities;
 
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
 
 import java.util.List;
 
-//@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class UsersResponse {
 
     public UsersResponse() {}
@@ -18,44 +19,4 @@ public class UsersResponse {
     private String total_pages;
 
     private List<UserResponse.UserData> data;
-
-    public String getPage() {
-        return page;
-    }
-
-    public String getPerPage() {
-        return per_page;
-    }
-
-    public String getTotal() {
-        return total;
-    }
-
-    public String getTotalPages() {
-        return total_pages;
-    }
-
-    public void setPage(String page) {
-        this.page = page;
-    }
-
-    public void setPer_page(String per_page) {
-        this.per_page = per_page;
-    }
-
-    public void setTotal(String total) {
-        this.total = total;
-    }
-
-    public void setTotal_pages(String total_pages) {
-        this.total_pages = total_pages;
-    }
-
-    public void setData(List<UserResponse.UserData> data) {
-        this.data = data;
-    }
-
-    public List<UserResponse.UserData> getData() {
-        return data;
-    }
 }
