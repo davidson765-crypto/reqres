aaf70f533ac5beb1f942d5a3e7b8bf05a811deb0
davidson765-crypto <dmiraev@gmail.com>
2026-07-22 14:06:23 +0300

    remove hashmap

--- src/test/java/org/example/users/TestGetUserResponse.java
+++ src/test/java/org/example/users/TestGetUserResponse.java
@@ -6,32 +6,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.ParameterizedTest;
 
-import java.util.HashMap;
-
 public class TestGetUserResponse {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/users.csv", numLinesToSkip = 1)
     public void testGetUser(String id, String email, String first_name, String last_name, String avatar) {
 
-        UserResponse.UserData userData = new Requests().setSpecs().getUser(id, 200).getData();
+        UserResponse.UserData user_actual = new Requests().setSpecs().getUser(id, 200).getData();
 
-        HashMap<String, String> userBodyExpected = new HashMap<>();
+        UserResponse.UserData user_expected = new UserResponse.UserData()
+                .setId(id)
+                .setEmail(email)
+                .setAvatar(avatar)
+                .setFirst_name(first_name)
+                .setLast_name(last_name);
 
-        userBodyExpected.put("id", id);
-        userBodyExpected.put("email", email);
-        userBodyExpected.put("first_name", first_name);
-        userBodyExpected.put("last_name", last_name);
-        userBodyExpected.put("avatar", avatar);
-
-        HashMap<String, String> userBodyActual = new HashMap<>();
-
-        userBodyActual.put("id", userData.getId());
-        userBodyActual.put("email", userData.getEmail());
-        userBodyActual.put("first_name", userData.getFirst_name());
-        userBodyActual.put("last_name", userData.getLast_name());
-        userBodyActual.put("avatar", userData.getAvatar());
-
-        Assertions.assertEquals(userBodyActual, userBodyExpected);
+        Assertions.assertEquals(user_actual, user_expected);
     }
 }
