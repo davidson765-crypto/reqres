fcdcc99dfaa88d7e29fd9fef36afda67d613a1fb
davidson765-crypto <dmiraev@gmail.com>
2026-08-08 20:44:16 +0300

    move create user logic into extension

---
+++ src/main/java/org/example/extensions/UserExtension.java
@@ -0,0 +1,27 @@
+package org.example.extensions;
+
+import org.example.PropertyReadHelper;
+import org.example.entities.request.RegisterLoginRequestBody;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class UserExtension implements ParameterResolver {
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+
+        return parameterContext.getParameter()
+                .getType()
+                .equals(RegisterLoginRequestBody.class);
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+
+        return RegisterLoginRequestBody.builder()
+                .email(PropertyReadHelper.getPropertyCustom("API_EMAIL"))
+                .password(PropertyReadHelper.getPropertyKey("API_REGISTRATION_PASSWORD"))
+                .build();
+    }
+}
