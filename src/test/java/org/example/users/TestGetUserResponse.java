7f3a5be52b1fd78cc8add932604f3272e8d0b428
davidson765-crypto <my.ownastro@gmail.com>
2026-09-10 16:36:24 +0300

    Revert "Changes"
    This reverts commit 89ba87a1bbaa05ddc60bea5e6c5fe8feec125e4a.

--- src/test/java/org/example/users/TestGetUserResponse.java
+++ src/test/java/org/example/users/TestGetUserResponse.java
@@ -1,4 +1,4 @@
-package org.example.users.api.regression;
+package org.example.users;
 
 import io.qameta.allure.*;
 import org.example.entities.response.UserResponse;
--- src/test/java/org/example/users/TestRegisterUserResponse.java
+++ src/test/java/org/example/users/TestRegisterUserResponse.java
@@ -1,4 +1,4 @@
-package org.example.users.api.smoke;
+package org.example.users;
 
 import Base.WireMockHost;
 import com.github.tomakehurst.wiremock.client.WireMock;
